483c512833d10a975841f56ebab37a63d07facdb
Punit-Choudhary <itspunitchoudhary@gmail.com>
2021-04-14 12:25:43 +0530

    added replies for thanks

--- misc.py
+++ misc.py
@@ -2,9 +2,12 @@
 
 import datetime
 import pyjokes
+import random
 
 import voice
 
+thank_keys = ['thank you', 'thanks', "that's so sweet of you"]
+thank_reply = ['No Problem', 'Happy to help', "That's what i am here for", "My plesure"]
 
 def misc(query):
 
@@ -15,5 +18,9 @@ def misc(query):
     elif 'joke' in query.lower():
         joke = pyjokes.get_joke()
         voice.speak(joke)
+    
+    elif any([k in query.lower() for k in thank_keys]):
+        voice.speak(random.choice(thank_reply))
+
     else:
         voice.speak("Aah man, what are you talking about?")
